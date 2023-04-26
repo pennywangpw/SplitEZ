e87804eb9ebd60f9a539f97fa329f0e4a695620a
penny_w <penny0love.hotmail.com>
2023-04-26 13:42:10 -0700

    update seeds

--- app/seeds/expenses.py
+++ app/seeds/expenses.py
@@ -6,11 +6,19 @@ def seed_expenses():
     gocery1 = Expense(name='Trader Joe', expense_total=40, payer_user_id=1)
     gocery2 = Expense(name='Costco', expense_total=130, payer_user_id=2)
     gocery3 = Expense(name='H mart', expense_total=68, payer_user_id=1)
+    gocery4 = Expense(name='hotel', expense_total=345.69, payer_user_id=1,group_id= 1)
+    gocery5 = Expense(name='ticket', expense_total=288.30, payer_user_id=1, group_id =1)
+    gocery6 = Expense(name='bottle', expense_total=22.12, payer_user_id=1, group_id= 2)
+
+
 
 
     db.session.add(gocery1)
     db.session.add(gocery2)
     db.session.add(gocery3)
+    db.session.add(gocery4)
+    db.session.add(gocery5)
+    db.session.add(gocery6)
     db.session.commit()
 
 
