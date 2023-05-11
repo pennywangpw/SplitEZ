00c80a5ed70735b40bcdb80183e88477359df000
penny_w <penny0love.hotmail.com>
2023-05-11 16:32:37 -0700

    update seeds

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -10,10 +10,15 @@ def seed_users():
         username='marnie', email='marnie@aa.io', password='password')
     bobbie = User(
         username='bobbie', email='bobbie@aa.io', password='password')
+    alice = User(
+        username='alice', email='alice@aa.io', password='password')
+
 
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
+    db.session.add(alice)
+
     db.session.commit()
 
 
@@ -28,5 +33,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
