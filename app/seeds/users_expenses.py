910e61713d6136c6528308f161a338d05d758488
penny_w <penny0love.hotmail.com>
2023-07-12 16:05:09 -0700

    work on user_expenses model and seeds

--- app/seeds/users_expenses.py
+++ app/seeds/users_expenses.py
@@ -4,11 +4,19 @@ from app.models.users_expenses import users_expenses
 from sqlalchemy.sql import text
 
 
-def seed_tables():
+def seed_users_expenses_tables():
     user_expense_data = [
         {'owe_id': 1, 'expense_id':1},
         {'owe_id': 1, 'expense_id':2},
+        {'owe_id': 1, 'expense_id':3},
         {'owe_id': 2, 'expense_id':3},
+        {'owe_id': 2, 'expense_id':4},
+        {'owe_id': 2, 'expense_id':5},
+        {'owe_id': 2, 'expense_id':6},
+        {'owe_id': 2, 'expense_id':7},
+        {'owe_id': 2, 'expense_id':8},
+        {'owe_id': 2, 'expense_id':10},
+
     ]
 
     user_expense_rows = []
