eac68655e3260bd3edbb9f761e6fa773f1102fc4
penny_w <penny0love.hotmail.com>
2023-07-31 19:47:10 -0700

    work on backend expense validation

--- app/forms/expense_form.py
+++ app/forms/expense_form.py
@@ -6,11 +6,27 @@ wtforms_json.init()
 
 
 class ExpenseForm(FlaskForm):
+    #validate expense name
     name = StringField('name', validators=[DataRequired()])
+    #validate expense date
     expense_date = DateField('expense_date')
+    #validate expense total
     expense_total = DecimalField('expense_total', validators=[DataRequired()])
+    #validate payer_user_id
     payer_user_id = IntegerField('payer_user_id', validators=[DataRequired()])
+    #validate payer_user_id
+
     group_id = IntegerField('group_id')
+
+#validate debtor
+class Debotrdetail(FlaskForm):
+    class Meta:
+        csrf = False
+    debtor_id = IntegerField('debtor_id',validators=[DataRequired()])
+    owe_Amount = DecimalField('owe_Amount', validators=[DataRequired()])
+
+class Debtor(FlaskForm) :
+    debtors_info = FieldList(FormField(Debotrdetail),min_entries=1)
 
 
 # class Debotrdetail(FlaskForm):
@@ -24,15 +40,6 @@ class ExpenseForm(FlaskForm):
 #     debtor_info = FormField(Debotrdetail)
 
 
-class Debotrdetail(FlaskForm):
-    class Meta:
-        csrf = False
-    debtor_id = IntegerField('debtor_id',validators=[DataRequired()])
-    owe_Amount = DecimalField('owe_Amount', validators=[DataRequired()])
-
-class Debtor(FlaskForm) :
-    debtor_name = StringField()
-    debtors_info = FieldList(FormField(Debotrdetail),min_entries=1)
 
 
 
