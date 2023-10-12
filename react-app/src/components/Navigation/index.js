a976eca89b0fa9e9e1194b8fc924f5cefb376bcd
penny_w <penny0love.hotmail.com>
2023-10-12 10:44:21 -0700

    wor on css

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -9,7 +9,7 @@ function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<div className='bg-5cc5a7 pad-tb-25p min-width-540px'>
+		<div className='bg-5cc5a7 pad-tb-25p min-width-600px'>
 
 			{isLoaded && !sessionUser && (
 				<ul className='flx-jc-fe mrg20p '>
--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -8,19 +8,19 @@ function SplashPage() {
 
     return (
         <>
-            <div className="backgroud-img">
-                <div className="flx">
+            <div className="backgroud-img height-80-per min-width-600px">
+                <div className="flx mrg-0-auto">
                     <div className="welcome margin-30px-left-right">
                         <h2> Welcome to SplitEZ !</h2>
                         <div className="text-align-j">We understand your pain point - no body wants to do the math when hangs out with your friends, splitting bills can be a cumbersome process, and no one wants to spend time doing calculations. This application would be your best partner to make it easier for you to manage your spending.</div>
                         {/* <h3 className="welcome">SplitEZ makes it easy to split bills with friends and family!!</h3> */}
-                        <img src={"https://www.louisianafcu.org/hs-fs/hubfs/Blog_The%20polite%20persons%20guide%20to%20splitting%20the%20bill%20(558%20%C3%97%20325%20px).png?width=837&name=Blog_The%20polite%20persons%20guide%20to%20splitting%20the%20bill%20(558%20%C3%97%20325%20px).png"} alt="img" />
+                        <img className="max-width-100" src={"https://www.louisianafcu.org/hs-fs/hubfs/Blog_The%20polite%20persons%20guide%20to%20splitting%20the%20bill%20(558%20%C3%97%20325%20px).png?width=837&name=Blog_The%20polite%20persons%20guide%20to%20splitting%20the%20bill%20(558%20%C3%97%20325%20px).png"} alt="img" />
                     </div>
                 </div>
 
 
             </div>
-            <footer>
+            <footer className="height-20-per min-width-600px">
                 <small>
                     Developer:
                     <div className="flx">
