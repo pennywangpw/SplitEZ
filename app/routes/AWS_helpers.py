954a31b97dc65850d3cb988b79b8433ec8d2d185
penny_w <penny0love.hotmail.com>
2023-12-13 22:45:34 -0800

    check images api

--- app/routes/AWS_helpers.py
+++ app/routes/AWS_helpers.py
@@ -21,6 +21,7 @@ def get_unique_filename(filename):
     return f"{unique_filename}.{ext}"
 
 def upload_file_to_s3(file, acl="public-read"):
+    print(f"---file Content Type: {file.content_type}")
     try:
         s3.upload_fileobj(
             file,
